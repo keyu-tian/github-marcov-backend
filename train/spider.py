156c23bcc4ee1796cba28b7ede6a77da93d3074f
tky_hm_xps13h <k_code@163.com>
2021-07-04 17:21:29 +0800

    [upd] 更新火车站爬虫

--- train/spider.py
+++ train/spider.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 import json
 import requests
@@ -16,7 +17,7 @@ django.setup()
 
 
 from country.models import Country, City
-from train.models import *
+from train.models import Train, Station, MidStation
 
 # 暂时不管Gxxx/Gxxx格式的版本
 from requests.exceptions import Timeout
@@ -25,8 +26,6 @@ from utils.cast import address_to_jingwei
 URL = 'http://train.qunar.com/qunar/checiInfo.jsp'
 DEFAULT_DATE = datetime.datetime.now()
 DEFAULT_DATE_STR = DEFAULT_DATE.strftime('%Y-%m-%d')  # 默认查询当天的
-
-logger = create_logger(logger_name='crawler', log_path='logging/crawler.log', to_stdout=False)
 
 mingming = [('G', 9999),
             ('C', 9999),
@@ -37,7 +36,7 @@ mingming = [('G', 9999),
             ('Y', 999)]
 
 
-def getinfo(train_number='G99', date=DEFAULT_DATE_STR):
+def getinfo(logger, train_number='G99', date=DEFAULT_DATE_STR):
     logger.info('getinfo')
     params = {
         'method_name': 'buy',
@@ -63,7 +62,9 @@ def getinfo(train_number='G99', date=DEFAULT_DATE_STR):
     return result
 
 
-def main(index=0, st=1):
+def main(path, index=0, st=1):
+    logger = create_logger(logger_name='spider', log_path=os.path.join(path, 'spider.log'), to_stdout=False)
+    
     country, flag = Country.objects.get_or_create(name_ch='中国', defaults={'name_en': 'Chinese'})
     for a in range(index, len(mingming)):
         for i in range(st, mingming[a][1]):
@@ -72,7 +73,7 @@ def main(index=0, st=1):
             else:
                 time.sleep(3)
             name = mingming[a][0] + str(i)
-            result = getinfo(train_number=name)  # result是json
+            result = getinfo(logger=logger, train_number=name)  # result是json
             if result and not Train.objects.filter(name=name).exists():
                 dept_city_name = result.get('trainInfo').get(name).get('deptCity')
                 dept_sta_name = result.get('trainInfo').get(name).get('deptStation')
@@ -82,29 +83,35 @@ def main(index=0, st=1):
                 arri_sta_name = result.get('trainInfo').get(name).get('deptStation')
                 arri_time = result.get('trainInfo').get(name).get('deptTime')
                 arri_date = result.get('trainInfo').get(name).get('deptTime')
+                
                 dept_city, flag = City.objects.get_or_create(name_ch=dept_city_name)
-                if flag:
+                if flag:  # 数据库没有的新的国家，存名字
                     dept_city.country = country
                     dept_city.save()
                 dept_sta, flag = Station.objects.get_or_create(name_cn=dept_sta_name)
-                if flag:  # 是新建，存经纬度
+                if flag:  # 数据库没有的新的火车站，存经纬度
                     dept_sta.jingdu, dept_sta.weidu = address_to_jingwei(dept_sta_name + '站')
                     dept_sta.city = dept_city
                     dept_sta.save()
+                
                 arri_city, flag = City.objects.get_or_create(name_ch=arri_city_name)
-                if flag:
+                if flag:  # 数据库没有的新的国家，存名字
                     arri_city.country = country
                     arri_city.save()
                 arri_sta, flag = Station.objects.get_or_create(name_cn=arri_sta_name)
-                if flag:  # 是新建，存经纬度
+                if flag:  # 数据库没有的新的火车站，存经纬度
                     arri_sta.jingdu, arri_sta.weidu = address_to_jingwei(arri_sta_name + '站')
                     arri_sta.city = arri_city
                     arri_sta.save()
-                train, flag = Train.objects.get_or_create(name=name, defaults={'dept_date': dept_date,
-                                                                               'dept_time': dept_time,
-                                                                               'arri_date': arri_date,
-                                                                               'arri_time': arri_time})
-                if flag:
+                
+                train, flag = Train.objects.get_or_create(
+                    name=name, defaults={
+                        'dept_date': dept_date,
+                        'dept_time': dept_time,
+                        'arri_date': arri_date,
+                        'arri_time': arri_time
+                    })
+                if flag:  # 数据库没有的新的列车，存储数据
                     train.interval = result.get('extInfo').get('allTime')
                     train.kilometer = result.get('extInfo').get('allMileage')
                     mid_list = result.get('trainScheduleBody')
@@ -118,27 +125,24 @@ def main(index=0, st=1):
                         MidStation.objects.create(index=mid_list.index(c) + 1, arri_date=content[2],
                                                   arri_time=content[3], station=sta, train=train)
                 
-                gotfile = open('../data/火车班次json数据.json', 'a', encoding='utf-8')
-                gotfile.write(str(result) + '\n')
-                gotfile.close()
-                
-                gotfile = open('../data/火车班次列表.json', 'a', encoding='utf-8')
-                gotfile.write(name + '\n')
-                gotfile.close()
+                with open(os.path.join(path, '火车班次json数据.json'), 'a', encoding='utf-8') as fp:
+                    fp.write(str(result) + '\n')
+                with open(os.path.join(path, '火车班次列表.json'), 'a', encoding='utf-8') as fp:
+                    fp.write(name + '\n')
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train-Spider')
+    parser.add_argument('path', default='train_crawler', type=str)
+    parser.add_argument('index', default=0, type=str, required=True)
+    parser.add_argument('st', default=1, type=str, required=True)
+    args = parser.parse_args()
+    
     start = str(datetime.datetime.now())
-    if len(sys.argv) > 1:
-        index = int(sys.argv[1])
-        st = int(sys.argv[2])
-        print('输入了参数 ' + str(index) + '和' + str(st) + ' 爬虫将会从该行开始抓取')
-        main(index, st)
-    else:
-        print('爬虫脚本已启动, 使用"tail -f ./data/火车班次json数据" 可以实时查看抓取的数据,开始时间为' + start)
-        main()
+    print(f'[{start}] 开始爬取，path={args.path}, index={args.index}, st={args.st}...')
+    main(args.path, args.index, args.st)
     end = str(datetime.datetime.now())
-    print('抓取完毕，请进入data文件夹查看数据， 进入log文件夹查看日志,开始时间为' + start + '结束时间为' + end)
+    print(f'抓取完毕，请进入{args.path}文件夹查看数据，进入spider.log文件夹查看日志；开始时间为' + start + '结束时间为' + end)
     
     # res = getinfo('G1317')
     # print(res)
