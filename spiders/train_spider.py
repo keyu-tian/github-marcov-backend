9fa83f18916c1587da4bb345c94f9b01f60d8bd0
tky_hm_xps13h <k_code@163.com>
2021-07-05 01:44:52 +0800

    [upd] 分布式列车爬虫预备

--- spiders/train_spider.py
+++ spiders/train_spider.py
@@ -4,7 +4,7 @@ import json
 import os
 
 import requests
-import sys
+from tqdm import tqdm
 import time
 from utils.log import create_logger
 # 暂时不管Gxxx/Gxxx格式的版本
@@ -24,7 +24,7 @@ mingming = [('G', 9999),
 
 
 def getinfo(logger, train_number='G99', date=DEFAULT_DATE_STR):
-    logger.info('getinfo')
+    # logger.info('getinfo')
     params = {
         'method_name': 'buy',
         'ex_track': '',
@@ -49,39 +49,62 @@ def getinfo(logger, train_number='G99', date=DEFAULT_DATE_STR):
     return result
 
 
-def main(path, index=0, st=1):
+def main(verbose, path, index=0, st=1):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    logger = create_logger(logger_name='spider', log_path=os.path.join(path, 'spider.log'), to_stdout=False)
+    logger = create_logger(logger_name=os.path.basename(path), log_path=os.path.join(path, f'log.log'), to_stdout=False)
 
-    for a in range(index, len(mingming)):
-        for i in range(st, mingming[a][1]):
+    for current_index in range(index, len(mingming)):
+        if verbose:
+            bar = tqdm(range(st, mingming[current_index][1]), dynamic_ncols=True)
+        else:
+            bar = range(st, mingming[current_index][1])
+        for current_st in bar:
             if 0 <= datetime.datetime.now().hour <= 6:
                 time.sleep(0.5)
             else:
                 time.sleep(1)
-            name = mingming[a][0] + str(i)
+            name = mingming[current_index][0] + str(current_st)
             result = getinfo(logger=logger, train_number=name)  # result是json
-            print(result)
+            # print(result)
             if result:
+                data = list(result['trainInfo'].values())[0]
+                if verbose:
+                    bar.set_description(f'[{name}]')
+                    bar.set_postfix_str(f'{data["deptCity"]} => {data["arriCity"]}')
+                with open(os.path.join(path, '火车班次爬到哪了.txt'), 'w', encoding='utf-8') as fp:
+                    fp.write(f'index={current_index}, st={current_st+1}')
                 with open(os.path.join(path, '火车班次json数据.json'), 'a', encoding='utf-8') as fp:
                     fp.write(json.dumps(result) + '\n')
                 with open(os.path.join(path, '火车班次列表.json'), 'a', encoding='utf-8') as fp:
                     fp.write(name + '\n')
+            else:
+                if verbose:
+                    bar.set_description(f'[{name}]: failed')
+                    bar.set_postfix_str('failed')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train-Spider')
-    parser.add_argument('--path', required=False, default='train_crawler', type=str)
+    parser.add_argument('--verbose', required=False, default=True, type=bool)
+    parser.add_argument('--path', required=False, default=os.path.join('spiders_data', 'train_spider'), type=str)
     parser.add_argument('--index', required=False, default=0, type=int)
     parser.add_argument('--st', required=False, default=1, type=int)
     args = parser.parse_args()
 
-    start = str(datetime.datetime.now())
-    print(f'[{start}] 开始爬取，path={args.path}, index={args.index}, st={args.st}...')
-    main(args.path, args.index, args.st)
-    end = str(datetime.datetime.now())
+    # [todo]@tky: 分布式爬取在这里写分布式的初始化，记得 verbose 设为 is_master
+
+    start = datetime.datetime.now().strftime('[%m-%d %H:%M:%S]')
+    args.path += f'_index{args.index}_st{args.st}'
+    print(f'{start} 开始爬！path={args.path}, index={args.index}, st={args.st}...')
+    try:
+        main(args.verbose, args.path, args.index, args.st)
+    except:
+        with open(os.path.join(args.path, '火车班次爬到哪了.txt'), 'r', encoding='utf-8') as fp:
+            s = fp.read()
+            print(f'爬失败了，请你下次从 {s} 再开始爬！')
+    print(f'爬完了！')
 
     # res = getinfo('G1317')
     # print(res)
