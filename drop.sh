6df268202504f473536429c058b3728623af0ff3
tky_hm_xps13h <k_code@163.com>
2021-07-04 15:08:12 +0800

    [new] 添加requirements.txt、删库脚本、一键安装python包的脚本

---
+++ drop.sh
@@ -0,0 +1,15 @@
+# mysql -u root -p
+# ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+
+find . -name 'migrations' -type d -exec rm -rf {} +
+find . -name '__pycache__' -type d -exec rm -rf {} +
+
+function mys_call() {
+  l="$1"
+  mysql -h"localhost"  -P"3306"  -u"root" -e "${l}" --default-character-set=UTF8
+}
+
+db_name="marcovdb"
+mys_call "drop database ${db_name};" >/dev/null 2>&1
+mys_call "CREATE DATABASE ${db_name} default character set utf8mb4 COLLATE = utf8mb4_unicode_ci;"
+
