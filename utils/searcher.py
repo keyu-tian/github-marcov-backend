9dd4962b6214f2b778665edacee196bb71a273f5
tky_hm_xps13h <k_code@163.com>
2021-07-05 16:06:25 +0800

    [upd] 更新 README 解释各个脚本

--- utils/searcher.py
+++ utils/searcher.py
@@ -5,15 +5,16 @@ from colorama import Fore
 
 
 def walk_check(dirname, key):
-    os.chdir(dirname)
+    project_root_path = os.getcwd()
     lines = 0
-    for path, dirs, files in os.walk('.'):
+    for path, dirs, files in os.walk(dirname):
         if any(s in path for s in ['__pycache__', 'migrations', 'frontend']):
             continue
+        temp_cwd = os.getcwd()
+        os.chdir(path)
         for f_name in files:
             if not f_name.endswith('.py'):
                 continue
-            f_name = os.path.join(path, f_name)
             with open(f_name, encoding='utf-8') as fp:
                 contains = []
                 for i, line in enumerate(fp):
@@ -22,12 +23,15 @@ def walk_check(dirname, key):
                         contains.append(i+1)
                 if len(contains) == 0:
                     continue
-                print(f'`{key}` found in  {f_name:20s}:  [line {str(contains).strip("[]")}]')
+                rel_path = os.path.join(os.path.relpath(os.getcwd(), project_root_path), f_name)
+                print(f'`{key}` found in  {rel_path:30s}:  [line {str(contains).strip("[]")}]')
                 # print(f''
                 #       + Fore.GREEN + f'`{key}`' + Fore.RESET + ' found in '
                 #       + Fore.CYAN + f' {f_name:20s}' + Fore.RESET + ': '
                 #       + Fore.BLUE + f'[line {str(lines).strip("[]")}]' + Fore.RESET)
-    if dirname in ['.', './']:
+        os.chdir(temp_cwd)
+        
+    if dirname in ['.', './', '.\\']:
         print(f'\n[total lines of MarCov-19 backend: {lines}]')
 
 
