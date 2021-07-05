e3eefbda85662328e77ef15598aa00dee091c3b6
tky_hm_xps13h <k_code@163.com>
2021-07-05 15:33:07 +0800

    [upd] 更新脚本：drop.sh（用于重新建库）和 make_migs.sh（用于一键make migrations）

--- drop.sh
+++ drop.sh
@@ -5,11 +5,12 @@ find . -name 'migrations' -type d -exec rm -rf {} +
 find . -name '__pycache__' -type d -exec rm -rf {} +
 
 function mys_call() {
-  l="$1"
-  mysql -h"localhost"  -P"3306"  -u"root" -e "${l}" --default-character-set=UTF8
+  cmd="$1"
+  mysql -h"localhost"  -P"3306"  -u"root" -e "${cmd}" --default-character-set=UTF8
 }
 
 db_name="marcovdb"
+mys_call "set global time_zone='+8:00'"
 mys_call "drop database ${db_name};" >/dev/null 2>&1
-mys_call "CREATE DATABASE ${db_name} default character set utf8mb4 COLLATE = utf8mb4_unicode_ci;"
+mys_call "create database ${db_name} default character set utf8mb4 COLLATE = utf8mb4_unicode_ci;"
 
