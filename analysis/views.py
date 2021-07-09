031b33bd5f38a2e70fc72eeb5f18429853f3bc23
lucieniii <lucienli0320@gmail.com>
2021-07-09 23:12:13 +0800

    [fix] 国内国外数据分析确定采用json

--- analysis/views.py
+++ analysis/views.py
@@ -97,51 +97,6 @@ class SearchAnalyse(View):
                             'total_confirmed': c['total']['confirmed']
                         })
 
-
-        """
-        name_dict = {}
-        for it in country_dict.items():
-            name_dict[it[1]] = it[0]
-        try:
-            if kwargs['name'] in province_dict_ch.keys():
-                epidemics = HistoryEpidemicData.objects.filter(province_ch__exact=province_dict_ch[kwargs['name']])
-                population = province_population.get(kwargs['name'], '未知')
-            else:
-                epidemics = HistoryEpidemicData.objects.filter(country_ch__exact=name_dict[kwargs['name']])
-                population = '未知'
-                for it in country_population.items():
-                    # TODO: 查询国家人口模糊匹配
-                    if it[0] == name_dict[kwargs['name']]:
-                        population = it[1]
-        except:
-            return 7
-        
-        daily_data = {}
-        for epidemic in epidemics:
-            if epidemic.date not in daily_data.keys():
-                daily_data[epidemic.date] = {
-                    'date': epidemic.date,
-                    'total_died': 0,
-                    'total_cured': 0,
-                    'total_confirmed': 0
-                }
-            if kwargs['name'] in province_dict_ch.keys():
-                daily_data[epidemic.date] = {
-                    'date': epidemic.date,
-                    'total_died': max(epidemic.province_total_died, daily_data[epidemic.date]['total_died']),
-                    'total_cured': max(epidemic.province_total_cured, daily_data[epidemic.date]['total_cured']),
-                    'total_confirmed': max(epidemic.province_total_confirmed, daily_data[epidemic.date]['total_confirmed'])
-                }
-            else:
-                daily_data[epidemic.date] = {
-                    'date': epidemic.date,
-                    'total_died': epidemic.province_total_died + daily_data[epidemic.date]['total_died'],
-                    'total_cured': epidemic.province_total_cured + daily_data[epidemic.date]['total_cured'],
-                    'total_confirmed': epidemic.province_total_confirmed + daily_data[epidemic.date]['total_confirmed']
-                }
-        """
-
-
         return 0, population, daily_data
 
 
@@ -151,7 +106,6 @@ class CountryAnalyze(View):
         kwargs: dict = json.loads(request.body)
         if kwargs.keys() != {'name'}:
             return 1, []
-
 
         try:
             global_json_path = os.path.join(IMPORTER_DATA_DIRNAME, 'global.json')
@@ -182,69 +136,6 @@ class CountryAnalyze(View):
                             'total': c['total']
                         })
 
-        '''
-        name_dict = {}
-        for it in country_dict.items():
-            name_dict[it[1]] = it[0]
-        try:
-            if kwargs['name'] in province_dict_ch.keys():
-                epidemics = HistoryEpidemicData.objects.filter(province_ch__exact=province_dict_ch[kwargs['name']])
-                population = province_population.get(kwargs['name'], '未知')
-            else:
-                epidemics = HistoryEpidemicData.objects.filter(country_ch__exact=name_dict[kwargs['name']])
-                population = '未知'
-                for it in country_population.items():
-                    # TODO: 查询国家人口模糊匹配
-                    if it[0] == name_dict[kwargs['name']]:
-                        population = it[1]
-        except:
-            return 7
-
-        daily_data = {}
-        for epidemic in epidemics:
-            if epidemic.date not in daily_data.keys():
-                daily_data[epidemic.date] = {
-                    'date': epidemic.date,
-                    'total': {
-                        'died': 0,
-                        'cured': 0,
-                        'confirmed': 0
-                    },
-                    'new': {
-                        'died': 0,
-                        'cured': 0,
-                        'confirmed': 0
-                    }
-                }
-            if kwargs['name'] in province_dict_ch.keys():
-                daily_data[epidemic.date] = {
-                    'date': epidemic.date,
-                    'total': {
-                        'died': max(epidemic.province_total_died, daily_data[epidemic.date]['total']['died']),
-                        'cured': max(epidemic.province_total_cured, daily_data[epidemic.date]['total']['cured']),
-                        'confirmed': max(epidemic.province_total_confirmed, daily_data[epidemic.date]['total']['confirmed'])
-                    },
-                    'new': {
-                        'died': max(epidemic.province_new_died, daily_data[epidemic.date]['new']['died']),
-                        'cured': max(epidemic.province_new_cured, daily_data[epidemic.date]['new']['cured']),
-                        'confirmed': max(epidemic.province_new_confirmed, daily_data[epidemic.date]['new']['confirmed'])
-                    }
-                }
-            else:
-                daily_data[epidemic.date] = {
-                    'date': epidemic.date,
-                    'total': {
-                        'died': epidemic.province_total_died + daily_data[epidemic.date]['total']['died'],
-                        'cured': epidemic.province_total_cured + daily_data[epidemic.date]['total']['cured'],
-                        'confirmed': epidemic.province_total_confirmed + daily_data[epidemic.date]['total']['confirmed']
-                    },
-                    'new': {
-                        'died': epidemic.province_new_died + daily_data[epidemic.date]['new']['died'],
-                        'cured': epidemic.province_new_cured + daily_data[epidemic.date]['new']['cured'],
-                        'confirmed': epidemic.province_new_confirmed + daily_data[epidemic.date]['new']['confirmed']
-                    }
-                }
-        '''
         return 0, population, daily_data
 
 
