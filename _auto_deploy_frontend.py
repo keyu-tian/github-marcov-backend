38bf1b15f5168093e3806e1f6b108d5ecddc7a7f
tky_hm_xps13h <k_code@163.com>
2021-07-10 16:47:22 +0800

    [upd] 自动部署前端

--- _auto_deploy_frontend.py
+++ _auto_deploy_frontend.py
@@ -6,9 +6,9 @@ import colorama
 def main():
     colorama.init(autoreset=True)
     os.chdir('frontend')
-    newist_zip = sorted(filter(lambda s: s.endswith('.zip'), os.listdir()), key=os.path.getatime)[-1]
-    print(colorama.Fore.GREEN + '[newist_zip]: ' + colorama.Fore.WHITE + newist_zip)
-    os.system(f'rm -rf dist; unzip "{newist_zip}" >/dev/null 2>&1')
+    newest_zip = sorted(filter(lambda s: s.endswith('.zip'), os.listdir()), key=os.path.getatime)[-1]
+    print(colorama.Fore.GREEN + '[newest_zip]: ' + colorama.Fore.WHITE + newest_zip)
+    os.system(f'rm -rf dist; unzip "{newest_zip}" >/dev/null 2>&1')
     
 
 if __name__ == '__main__':
