abd552386e803ded45c21bb60716f48acf05e8ba
tky_hm_xps13h <k_code@163.com>
2021-07-10 17:17:04 +0800

    [upd] spiders 一键导入

--- spiders/__init__.py
+++ spiders/__init__.py
@@ -1,5 +1,7 @@
 import colorama
 
+from country.models import Country, City, Province
+
 
 def re_import(name, launch_spider, *args, **kwargs):
     if launch_spider:
@@ -14,6 +16,10 @@ def re_import(name, launch_spider, *args, **kwargs):
 def init_import(re_import_train=False):
     colorama.init(autoreset=True)
 
+    Country.objects.all().delete()
+    City.objects.all().delete()
+    Province.objects.all().delete()
+    
     re_import('risk', True)
     re_import('news', True, delete_old_data=True)
     re_import('dxy_news', False, delete_old_data=False) # 不launch是因为dxy_news没有爬虫，只有导入者
@@ -22,10 +28,16 @@ def init_import(re_import_train=False):
     if re_import_train:
         re_import('train', False, line_start=0)
     re_import('epidemic_domestic', False)
+    re_import('flight_once', False)
 
-    print(colorama.Fore.GREEN + '=> finished.')
+    print('')
+    print(colorama.Fore.WHITE + '=> finished.')
     
 
 def daily_import():
-    # todo
-    ...
+    re_import('flight_daily', False)
+    # todo:
+    # re_import('epidemic_domestic_daily', False)
+
+    print('')
+    print(colorama.Fore.WHITE + '=> finished.')
