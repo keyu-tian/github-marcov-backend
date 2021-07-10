11c16bf6c4a48ad2b23d2f1039a91558bb260625
tky_hm_xps13h <k_code@163.com>
2021-07-10 23:44:36 +0800

    [fix] epidemic_global_importer.py url忘记使用镜像，下不下来

--- spiders/epidemic_global_importer.py
+++ spiders/epidemic_global_importer.py
@@ -61,7 +61,7 @@ def epidemic_global_import(start_dt=None):
     vacc_file = os.path.join(IMPORTER_DATA_DIRNAME, 'vaccinations.csv')
     if os.path.exists(vacc_file):
         os.remove(vacc_file)
-    download_from_url('https://github.com/owid/covid-19-data/raw/master/public/data/vaccinations/vaccinations.csv', vacc_file)
+    download_from_url('https://github.com.cnpmjs.org/owid/covid-19-data/raw/master/public/data/vaccinations/vaccinations.csv', vacc_file)
     data_vaccinations = pandas.read_csv(vacc_file).fillna(0)
     
     requests.packages.urllib3.disable_warnings()
