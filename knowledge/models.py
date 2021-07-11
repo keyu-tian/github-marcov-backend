d24cbf20ccf330cd934a0a759a94453e4afd5795
HeySearra <heysearra@163.com>
2021-07-11 10:58:33 +0800

    [upd]小知识板块把数据文件上传了（很小）

--- knowledge/models.py
+++ knowledge/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Knowledge(models.Model):
     title = models.TextField()
     body = models.TextField()
+    source = models.CharField(max_length=128, default="未知")
 
--- spiders/small_knowledge_importer.py
+++ spiders/small_knowledge_importer.py
@@ -1,13 +1,24 @@
 import json
 import os
+# import marcov19.settings
+# from django.conf import settings
+# settings.configure(DEBUG=True, default_settings=marcov19.settings)
+# import os
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marcov19.settings')
+# import django
+# django.setup()
 from knowledge.models import Knowledge
 from meta_config import SPIDER_DATA_DIRNAME
 
 
 def small_knowledge_importer():
     Knowledge.objects.all().delete()
-    with open(os.path.join(SPIDER_DATA_DIRNAME, 'small_knowledge.json'), 'r', encoding='utf-8') as file:
+    with open(os.path.join('spiders', 'small_knowledge.json'), 'r', encoding='utf-8') as file:
         js = json.load(file)
     data = js['data']
     for a in data:
-        Knowledge.objects.create(title=a['title'], body=a['body'])
+        Knowledge.objects.create(title=a['title'], body=a['body'], source=a['source'])
+
+
+# if __name__ == '__main__':
+#     small_knowledge_importer()
