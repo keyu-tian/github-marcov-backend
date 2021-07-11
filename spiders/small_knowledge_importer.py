2fd1611d8a93b899ab7b3d521819bafa8a4ae533
HeySearra <heysearra@163.com>
2021-07-11 19:30:37 +0800

    [new]小知识json的path

--- spiders/small_knowledge_importer.py
+++ spiders/small_knowledge_importer.py
@@ -13,7 +13,7 @@ from meta_config import SPIDER_DATA_DIRNAME
 
 def small_knowledge_importer():
     Knowledge.objects.all().delete()
-    with open(os.path.join('spiders', 'small_knowledge.json'), 'r', encoding='utf-8') as file:
+    with open(os.path.join(SPIDER_DATA_DIRNAME, 'small_knowledge.json'), 'r', encoding='utf-8') as file:
         js = json.load(file)
     data = js['data']
     for a in data:
