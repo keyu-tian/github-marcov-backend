80ce93036748a11142262f4adcfc54be05d9b079
blog <wz1234@buaa.edu.cn>
2021-07-13 05:11:34 +0800

    [upd] 导入测试数据不需要删库拉

--- spiders/test_data_importer.py
+++ spiders/test_data_importer.py
@@ -72,27 +72,29 @@ def rand_content() -> str:
 
 
 def test_data_import():
-    User.objects.all().delete()
-    Question.objects.all().delete()
-    Tag.objects.all().delete()
-    Content.objects.all().delete()
+    # User.objects.all().delete()
+    # Question.objects.all().delete()
+    # Tag.objects.all().delete()
+    # Content.objects.all().delete()
     users = []
-    for i in range(1, 19):
+    user_num = User.objects.all().count()
+    for i in range(1+user_num, 19+user_num):
         u = User.objects.create(name=f'test{i}', account=f'account{i}', pwd=f'password{i}', identity=1, login_date='')
         users.append(u)
-    for i in range(19, 21):
+    for i in range(19+user_num, 21+user_num):
         u = User.objects.create(name=f'test{i}', account=f'account{i}', pwd=f'password{i}', identity=2, login_date='')
         users.append(u)
     questions = []
-    for i in range(1, 11):
-        q = Question.objects.create(title=f'question{i}', user=users[random.randint(1, 20)], published_time=datetime.now(), solved=random.randint(1, 10) > 8)
+    question_num = Question.objects.all().count()
+    for i in range(1+question_num, 11+question_num):
+        q = Question.objects.create(title=f'question{i}', user=users[random.randint(0, 19)], published_time=datetime.now(), solved=random.randint(1, 10) > 8)
         questions.append(q)
         if random.randint(0, 1) == 1:
             Tag.objects.create(question=q, name='tips')
         if random.randint(0, 1) == 1:
             Tag.objects.create(question=q, name='fake')
     for i in range(1, 100):
-        c = Content.objects.create(user=users[random.randint(1, 20)], content=rand_content(), floor=0, published_time=datetime.now(), question=questions[random.randint(1, 10)], is_top=random.randint(1, 100) > 90)
+        c = Content.objects.create(user=users[random.randint(0, 19)], content=rand_content(), floor=0, published_time=datetime.now(), question=questions[random.randint(0, 9)], is_top=random.randint(1, 100) > 90)
         c.question.replied_time = datetime.now()
         if c.user.identity == 2:
             c.question.expert_reply = True
