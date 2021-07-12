4e8398f39deff03d190779a34692de6c8007c3cb
HeySearra <heysearra@163.com>
2021-07-12 22:43:50 +0800

    [new]引定时任务相关包

--- marcov19/settings.py
+++ marcov19/settings.py
@@ -599,6 +599,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_crontab',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
