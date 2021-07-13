ed66da44b02704279451906c5cda542b2f557de8
HeySearra <heysearra@163.com>
2021-07-13 11:33:18 +0800

    [upd]train import

--- spiders/travel_policy_spider.py
+++ spiders/travel_policy_spider.py
@@ -61,7 +61,7 @@ def main():
                     if js['data']['city'] in a:
                         js['data']['city'] = a
                         break
-                with open(os.path.join(path, 'policy_by_city.json'), 'a', encoding='utf-8') as fp:
+                with open(os.path.join(SPIDER_DATA_DIRNAME, 'travel_policy_spider_all', 'policy_by_city.json'), 'a', encoding='utf-8') as fp:
                     fp.write(json.dumps({
                         'province': js['data']['province'],
                         'city': js['data']['city'],
